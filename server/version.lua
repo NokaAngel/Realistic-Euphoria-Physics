b3143e37b8c416f7793fd1ecc0fe4873adcb2dc3
NokaAngel <robert@theraccoon.dev>
2025-03-31 14:49:57 -0500

    Update version.lua

--- server/version.lua
+++ server/version.lua
@@ -10,39 +10,44 @@ local label =
   ||
   ]]  
 Citizen.CreateThread(function()
-	local CurrentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
-	if not CurrentVersion then
-		print('^1Realsitic Euphoria Physics Version Check Failed!^7')
-	end
+    local CurrentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
+    if not CurrentVersion then
+        print('^1Realistic Euphoria Physics Version Check Failed!^7')
+    end
 
-	function VersionCheckHTTPRequest()
-		PerformHttpRequest('https://raw.githubusercontent.com/PeEcEeChIP/version-history/version/realistic-euphoria-ragdoll.json', VersionCheck, 'GET')
-	end
+    function VersionCheckHTTPRequest()
+        PerformHttpRequest('https://versions.theraccoon.dev/version.php?resource=realistic-euphoria-physics', VersionCheck, 'GET')
+    end
 
-	function VersionCheck(err, response, headers)
-		Citizen.Wait(3000)
-		if err == 200 then
-			local Data = json.decode(response)
-			if CurrentVersion ~= Data.NewestVersion then
-				print( label )			
-				print('  ||    \n  ||    Realistic Euphoria Physics is outdated!')
-				print('  ||    Current version: ^2' .. Data.NewestVersion .. '^7')
-				print('  ||    Your version: ^1' .. CurrentVersion .. '^7')
-				print('  ||    Please download the lastest version from ^5' .. Data.DownloadLocation .. '^7')
-				if Data.Changes ~= '' then
-					print('  ||    \n  ||    ^5Changes: ^7' .. Data.Changes .. "\n^0  \\\\\n")
-				end
-			else
-				print( label )			
-				print('  ||    ^2Realistic Euphoria Physics is up to date!\n^0  ||\n  \\\\\n')
-			end
-		else
-			print( label )			
-			print('  ||    ^1There was an error getting the latest version information, if the issue persists contact RaccoonWX on Discord.\n^0  ||\n  \\\\\n')
-		end
-		
-		SetTimeout(60000000, VersionCheckHTTPRequest)
-	end
+    function VersionCheck(err, response, headers)
+        Citizen.Wait(3000)
+        if err == 200 then
+            local version, download, changes = response:match('([^|]+)|([^|]+)|(.+)')
+            if not version or version == 'error' then
+                print(label)
+                print('  ||    ^1Error fetching version data from server.\n^0  ||\n  \\\\\n')
+                return
+            end
+            if CurrentVersion ~= version then
+                print(label)
+                print('  ||    \n  ||    Realistic Euphoria Physics is outdated!')
+                print('  ||    Current version: ^2' .. version .. '^7')
+                print('  ||    Your version: ^1' .. CurrentVersion .. '^7')
+                print('  ||    Please download the latest version from ^5' .. download .. '^7')
+                if changes ~= '' then
+                    print('  ||    \n  ||    ^5Changes: ^7' .. changes .. "\n^0  \\\\\n")
+                end
+            else
+                print(label)
+                print('  ||    ^2Realistic Euphoria Physics is up to date!\n^0  ||\n  \\\\\n')
+            end
+        else
+            print(label)
+            print('  ||    ^1There was an error getting the latest version information, if the issue persists contact NokaAngel on Discord.\n^0  ||\n  \\\\\n')
+        end
+        
+        SetTimeout(60000, VersionCheckHTTPRequest) -- Adjusted to 60 seconds
+    end
 
-	VersionCheckHTTPRequest()
+    VersionCheckHTTPRequest()
 end)
