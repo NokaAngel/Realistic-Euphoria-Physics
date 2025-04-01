d6d22d8e67fb2c1d4f4d895cb7f003979425c901
NokaAngel <robert@theraccoon.dev>
2025-04-01 01:39:39 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'TheRaccoon'
 description 'This is to increase the euphoria physics trying to mimic GTA IV.'
-version 'v1.0.3'
+version 'v1.0.4'
 
 client_script 'client/client.lua'
 server_script {
